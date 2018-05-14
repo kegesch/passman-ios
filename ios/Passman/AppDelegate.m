754d9fb328bb8e218d122184bae05425eb78af22
Jonas Geschke <mail@yonny.de>
2018-05-14 22:46:50 +0200

    fixed LaunchScreen logo alignment and flash after launchscreen

--- ios/Passman/AppDelegate.m
+++ ios/Passman/AppDelegate.m
@@ -23,12 +23,18 @@
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-
+  
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  
+  UIView* launchScreenView = [[[NSBundle mainBundle]
+                               loadNibNamed:@"LaunchScreen" owner:self options:nil] objectAtIndex: 0];
+  launchScreenView.frame = self.window.bounds;
+  rootView.loadingView = launchScreenView;
+  
   return YES;
 }
 
